bddf2b849905ff7c64ccde93d0c6d291995f9706
Balag Bálint <89921503+datwalkerv@users.noreply.github.com>
2025-03-06 09:54:13 +0100

    Create szamosszeg.py

---
+++ szamosszeg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    n1 = int(input())
+    #n1 = int(sys.argv[1])
+    n2 = int(input())
+    #n2 = int(sys.argv[2])
+
+    print(n1+n2)
+
+if __name__ == "__main__":
+    main()
