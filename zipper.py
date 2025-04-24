c4d0e9250f87446d377f46f9eeda398f217f9ab0
Balag Bálint <bbalint139@mailbox.unideb.hu>
2025-04-24 08:20:13 +0200

    Create zipper.py

---
+++ zipper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    codes = list(range(ord('a'), ord('z') + 1))
+
+    for t in zip(chars, codes):
+        print(t)
+    print()
+
+if __name__ == "__main__":
+    main()
